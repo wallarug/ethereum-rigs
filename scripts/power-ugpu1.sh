3410da5ee08463bf80030cc9c095567e9e9d62a8
wallarug <wallarug@coliemore.com.au>
2021-04-01 18:50:07 +1100

    ugpu1 hacks

---
+++ scripts/power-ugpu1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo nvidia-smi -pl 130
+
+counter=0
+while [ $counter -le 5 ]
+do
+DISPLAY=:0 XAUTHORITY=/run/user/1000/gdm/Xauthority nvidia-settings -a [gpu:$counter]/GPUFanControlState=1
+((counter++))
+done
+
+counter=0
+while [ $counter -le 14 ]
+do
+DISPLAY=:0 XAUTHORITY=/run/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=90
+((counter++))
+done
