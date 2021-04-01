a3ba77f634e2519d39b9476ff75280e4b50d0ed8
wallarug <wallarug@coliemore.com.au>
2021-04-01 18:22:44 +1100

    fix script

--- scripts/power-ugpu0.sh
+++ scripts/power-ugpu0.sh
@@ -1,17 +1,18 @@
-sudo -s
-nvidia-smi -pl 300
+#!/bin/bash
+
+sudo nvidia-smi -pl 300
 
 counter=0
-while [ $counter -le 8 ]
+while [ $counter -le 7 ]
 do
-`DISPLAY=:0 XAUTHORITY=/run/user/1000/gdm/Xauthority nvidia-settings -a [gpu:$counter]/GPUFanControlState=1`
+DISPLAY=:0 XAUTHORITY=/run/user/1000/gdm/Xauthority nvidia-settings -a [gpu:$counter]/GPUFanControlState=1
 ((counter++))
 done
 
 counter=0
-while [ $counter -le 15 ]
+while [ $counter -le 14 ]
 do
-`DISPLAY=:0 XAUTHORITY=/run/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=90`
+DISPLAY=:0 XAUTHORITY=/run/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=90
 ((counter++))
 done
 
