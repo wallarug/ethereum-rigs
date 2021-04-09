b662fb9dc98bef7848057a2ea1679e0bd3763403
wallarug <wallarug@coliemore.com.au>
2021-04-09 17:28:09 +1000

    reduce fans on ugpu7 to 75

--- scripts/power-ugpu7.sh
+++ scripts/power-ugpu7.sh
@@ -16,6 +16,6 @@ done
 counter=0
 while [ $counter -le 7 ]
 do
-DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=90
+DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=75
 ((counter++))
 done
