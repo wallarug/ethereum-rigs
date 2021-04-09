bda7b5cdb1840efafb3c1cbd72ad6403ab0d3904
wallarug <wallarug@coliemore.com.au>
2021-04-09 17:33:05 +1000

    update pl for 1080ti

--- scripts/power-ugpu7.sh
+++ scripts/power-ugpu7.sh
@@ -2,8 +2,8 @@
 
 sudo nvidia-smi -i 0 -pl 120
 sudo nvidia-smi -i 1 -pl 120
-sudo nvidia-smi -i 3 -pl 130
-sudo nvidia-smi -i 2 -pl 130
+sudo nvidia-smi -i 3 -pl 135
+sudo nvidia-smi -i 2 -pl 135
 sudo nvidia-smi -i 4 -pl 120
 
 counter=0
