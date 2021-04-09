6e4b6c7b3a615b649575c99b55f6a41671f4b0dd
wallarug <wallarug@coliemore.com.au>
2021-04-09 17:06:14 +1000

    added script for ugpu7

---
+++ scripts/power-ugpu7.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo nvidia-smi -i 0 -pl 130
+sudo nvidia-smi -i 1 -pl 130
+sudo nvidia-smi -i 3 -pl 150
+sudo nvidia-smi -i 2 -pl 150
+sudo nvidia-smi -i 4 -pl 130
+
+counter=0
+while [ $counter -le 4 ]
+do
+DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [gpu:$counter]/GPUFanControlState=1
+((counter++))
+done
+
+counter=0
+while [ $counter -le 11 ]
+do
+DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=90
+((counter++))
+done
