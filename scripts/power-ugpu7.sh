c7f1ca78c418fa7319fa69c6cc9caaf8a97e8283
wallarug <wallarug@coliemore.com.au>
2021-04-09 17:27:44 +1000

    more power fine-turning

--- scripts/power-ugpu7.sh
+++ scripts/power-ugpu7.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo nvidia-smi -i 0 -pl 130
-sudo nvidia-smi -i 1 -pl 130
-sudo nvidia-smi -i 3 -pl 150
-sudo nvidia-smi -i 2 -pl 150
-sudo nvidia-smi -i 4 -pl 130
+sudo nvidia-smi -i 0 -pl 120
+sudo nvidia-smi -i 1 -pl 120
+sudo nvidia-smi -i 3 -pl 130
+sudo nvidia-smi -i 2 -pl 130
+sudo nvidia-smi -i 4 -pl 120
 
 counter=0
 while [ $counter -le 4 ]
