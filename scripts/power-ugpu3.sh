a6411bb4644c5a8ae03eb296a49c68d805d720b0
wallarug <wallarug@coliemore.com.au>
2021-04-09 18:39:56 +1000

    ugpu3

--- scripts/power-ugpu3.sh
+++ scripts/power-ugpu3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo nvidia-smi -i 0 -pl 130
-sudo nvidia-smi -i 1 -pl 130
-sudo nvidia-smi -i 2 -pl 130
+sudo nvidia-smi -i 0 -pl 120
+sudo nvidia-smi -i 1 -pl 120
+sudo nvidia-smi -i 2 -pl 120
 sudo nvidia-smi -i 3 -pl 300
 sudo nvidia-smi -i 4 -pl 300
 sudo nvidia-smi -i 5 -pl 300
@@ -17,6 +17,6 @@ done
 counter=0
 while [ $counter -le 11 ]
 do
-DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=90
+DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=85
 ((counter++))
 done
