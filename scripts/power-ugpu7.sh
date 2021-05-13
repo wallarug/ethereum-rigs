48b06239c3e7a491ee5b27e262033ba188d6f524
wallarug <wallarug@coliemore.com.au>
2021-05-13 15:22:16 +1000

    update ugpu7

--- scripts/power-ugpu7.sh
+++ scripts/power-ugpu7.sh
@@ -3,8 +3,7 @@
 sudo nvidia-smi -i 0 -pl 120
 sudo nvidia-smi -i 1 -pl 120
 sudo nvidia-smi -i 3 -pl 135
-sudo nvidia-smi -i 2 -pl 135
-sudo nvidia-smi -i 4 -pl 120
+sudo nvidia-smi -i 2 -pl 300
 
 counter=0
 while [ $counter -le 4 ]
