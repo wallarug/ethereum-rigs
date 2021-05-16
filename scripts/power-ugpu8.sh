a573afc438db92bf838d67cc9216d8d4e51cea31
wallarug <wallarug@coliemore.com.au>
2021-05-16 20:19:13 +1000

    added ugpu8

---
+++ scripts/power-ugpu8.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo nvidia-smi -i 0 -pl 135
+sudo nvidia-smi -i 2 -pl 135
+sudo nvidia-smi -i 1 -pl 120
+
+counter=0
+while [ $counter -le 2 ]
+do
+DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [gpu:$counter]/GPUFanControlState=1
+((counter++))
+done
+
+counter=0
+while [ $counter -le 6 ]
+do
+DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=75
+((counter++))
+done
