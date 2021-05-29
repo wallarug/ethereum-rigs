df61b481db7904bd781091a5abebb9c90ff3aeb9
wallarug <wallarug@coliemore.com.au>
2021-05-29 16:47:30 +1000

    Update power-ugpu8.sh

--- scripts/power-ugpu8.sh
+++ scripts/power-ugpu8.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-sudo nvidia-smi -i 0 -pl 135
-sudo nvidia-smi -i 2 -pl 135
-sudo nvidia-smi -i 1 -pl 120
+sudo nvidia-smi -i 0 -pl 300
+sudo nvidia-smi -i 1 -pl 300
+sudo nvidia-smi -i 2 -pl 120
+sudo nvidia-smi -i 3 -pl 300
 
 counter=0
-while [ $counter -le 2 ]
+while [ $counter -le 3 ]
 do
 DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [gpu:$counter]/GPUFanControlState=1
 ((counter++))
 done
 
 counter=0
-while [ $counter -le 3 ]
+while [ $counter -le 14 ]
 do
 DISPLAY=:0 XAUTHORITY=un/user/1000/gdm/Xauthority nvidia-settings -a [fan:$counter]/GPUTargetFanSpeed=75
 ((counter++))
